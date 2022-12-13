b71b4c7d416b0009f1edf5f8bc0cc4b451abddb1
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-13 16:43:44 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true do
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :nick_name
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name_katakana, format: {with: /\A[ァ-ヶー]+\z/ }
+    validates :first_name_katakana, format: {with: /\A[ァ-ヶー]+\z/ }
+    validates :birth_day
+  end
 end
