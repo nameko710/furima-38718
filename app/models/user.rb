1c7e8277973e0f0bfdc20a380a359a4b0c1db882
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-13 18:05:02 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
   with_options presence: true do
     VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates :nick_name
-    validates :password, format: { with: VALID_PASSWORD_REGEX }
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :last_name_katakana, format: {with: /\A[ァ-ヶー]+\z/ }
-    validates :first_name_katakana, format: {with: /\A[ァ-ヶー]+\z/ }
+    validates :password, format: {with: VALID_PASSWORD_REGEX, message: "is invalid. Input alphanumeric characters."}
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name_katakana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_katakana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birth_day
   end
 end
