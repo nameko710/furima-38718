85a38a9b81664ff20955e1542506f1de7951215a
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-13 12:36:23 +0900

    Revert "userモデルのバリデーションを記述"
    This reverts commit 3777b5debcfbbae7f3bb8ad8d18f764f23c5eb2d.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nick_name presence: true
-  validates :last_name presence: true
-  validates :first_name presence: true
-  validates :last_name_katakana presence: true
-  validates :first_name_katakana presence: true
-  validates :birth_day presence: true
 end
