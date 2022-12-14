646b041197a7b6391050987bd14db4e0e621186c
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-14 18:50:51 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password,
+  format: { with: VALID_PASSWORD_REGEX, allow_blank: true, message: 'is invalid. Input alphanumeric characters.' }
+
   with_options presence: true do
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates :nick_name
-    validates :password,
-              format: { with: VALID_PASSWORD_REGEX, allow_blank: true, message: 'is invalid. Input alphanumeric characters.' }
     validates :last_name,
               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true, message: 'is invalid. Input full-width characters.' }
     validates :first_name,
