79f46bb7951cb322490de90a59a1ae180887c909
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-15 12:25:43 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
+
+  validates :item_name, :description, :price, presence: true
+  validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :duration_id,
+             numericality: { other_than: 1 }
+
   belongs_to :user
   belongs_to :category
   belongs_to :condition
