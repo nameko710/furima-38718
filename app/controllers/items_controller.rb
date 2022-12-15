375d86616ab03b6e4bef690e5560af26e5191e0f
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-15 17:04:08 +0900

    商品データの保存機能を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,24 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(items_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
   end
+
+  def items_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :duration_id, :price).merge(user_id: current_user.id)
+  end
+
 end
