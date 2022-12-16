5f9ba7126d18151491e1892179b98cc84416ceaa
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-16 12:28:42 +0900

    itemモデルの単体テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,18 @@
 FactoryBot.define do
   factory :item do
+    item_name {Faker::Games::Pokemon.name}
+    description {Faker::Games::Pokemon.move}
+    category_id {Faker::Number.between(from: 2, to: 11)}
+    condition_id {Faker::Number.between(from: 2, to: 7)}
+    delivery_charge_id {Faker::Number.between(from: 2, to: 3)}
+    prefecture_id {Faker::Number.between(from: 2, to: 48)}
+    duration_id {Faker::Number.between(from: 2, to: 4)}
+    price {Faker::Number.between(from: 300, to: 9999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
