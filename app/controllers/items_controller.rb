33e5814393e8ec906d63d0fe046c92f69a96e604
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-18 16:28:00 +0900

    ログイン状況に応じた遷移制限を作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -41,4 +42,12 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :delivery_charge_id,
                                  :prefecture_id, :duration_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    prototype = Item.find(params[:id])
+    unless current_user.id == prototype.user_id
+      redirect_to root_path
+    end
+  end
+
 end
