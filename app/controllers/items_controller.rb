8a46c4afdf4e61762c1b6f1e1a94bb4f4daa5d45
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-18 18:12:59 +0900

    itemsコントローラーbeforeアクションの変数名を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update, :move_to_index]
   before_action :move_to_index, only: [:edit]
-  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -41,13 +41,12 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :duration_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    prototype = Item.find(params[:id])
-    redirect_to root_path unless current_user.id == prototype.user_id
-  end
-
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
 end
