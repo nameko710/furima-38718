4dbd8b689a4fb0f6c1fe17db97b5a0166ab1290d
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-18 17:59:17 +0900

    itemsコントローラーの共通の記述をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(items_params)
       redirect_to item_path(@item.id)
     else
@@ -47,4 +45,9 @@ class ItemsController < ApplicationController
     prototype = Item.find(params[:id])
     redirect_to root_path unless current_user.id == prototype.user_id
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
