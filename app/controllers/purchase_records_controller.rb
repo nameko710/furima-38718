57893b5c0f50f05866783730b4028fc4cef8a1ae
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-19 17:27:51 +0900

    商品購入情報を保存する機能を作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -3,4 +3,21 @@ class PurchaseRecordsController < ApplicationController
     @purchase_record = PurchaseRecord.new
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_record = PurchaseRecord.create(purchase_record_params)
+    DeliveryAddress.create(delivery_address_params)
+    redirect_to root_path
+  end
+
+  def purchase_record_params
+    params.permit().merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def delivery_address_params
+    params.permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(purchase_record_id: @purchase_record.id)
+  end
+
+                        
 end
