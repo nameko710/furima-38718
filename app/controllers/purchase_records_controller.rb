67a05bcbacca80c9c2cd196a240b44224f96f2b3
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-20 14:34:54 +0900

    売却履歴とログイン状況に応じた遷移と表示の分岐を作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,11 +1,14 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :check_sold, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+  
   def index
     @purchase_address = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_record_params)
     if @purchase_address.valid?
        pay_item
@@ -15,6 +18,8 @@ class PurchaseRecordsController < ApplicationController
       render :index
     end
   end
+
+  private
 
   def purchase_record_params
     params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
@@ -28,5 +33,21 @@ class PurchaseRecordsController < ApplicationController
       currency: 'jpy' 
     )
   end
-                   
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+                  
+  def check_sold
+    if PurchaseRecord.exists?(item_id: @item.id)
+      redirect_to root_path
+    end
+  end
+
+  def move_to_index
+    if current_user.id == @item.user_id
+    redirect_to root_path 
+    end
+  end
+
 end
