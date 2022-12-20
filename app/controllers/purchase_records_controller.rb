5acccb4b9856cc2d0b9c6a38664431e4fa11ed31
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-20 11:25:37 +0900

    クレジットカード決済機能の作成

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -8,6 +8,7 @@ class PurchaseRecordsController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_record_params)
     if @purchase_address.valid?
+       pay_item
        @purchase_address.save
        redirect_to root_path
     else
@@ -16,7 +17,16 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def purchase_record_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price, 
+      card: purchase_record_params[:token], 
+      currency: 'jpy' 
+    )
   end
                    
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,8 +1,9 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number
+  attr_accessor :token, :user_id, :item_id, :post_code, :prefecture_id, :municipality, :address, :building_name, :phone_number
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
