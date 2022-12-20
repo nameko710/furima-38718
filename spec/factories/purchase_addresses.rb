3b8163b3d0faf9d24645bfbbd02a6c4cbbebdc2f
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-20 17:22:14 +0900

    テストコードの作成

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_address do
+    token {"tok_abcdefghijk00000000000000000"}
+    post_code { '123-4567' }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    municipality { Gimei.address.city.kanji }
+    address { Faker::JapaneseMedia::OnePiece.sea }
+    building_name { Faker::JapaneseMedia::OnePiece.island }
+    phone_number { Faker::Number.number(digits: 10) }
+  end
+end
