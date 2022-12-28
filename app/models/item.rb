22f9eba52cb54e91fda3a9e85030e098d3f85193
nameko710 <118578225+nameko710@users.noreply.github.com>
2022-12-28 14:43:01 +0900

    エラーメッセージを日本語表記に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,9 @@ class Item < ApplicationRecord
 
   validates :image, :item_name, :description, presence: true
   validates :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :duration_id,
-            numericality: { other_than: 1 }
+            numericality: { other_than: 1, message: :not_selected }
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, allow_blank: true, message: 'is invalid' }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, allow_blank: true}
 
   belongs_to :user
   belongs_to :category
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -6,12 +6,12 @@ class PurchaseAddress
     validates :token
     validates :user_id
     validates :item_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, allow_blank: true, message: :include_hyphens }
     validates :municipality
     validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, allow_blank: true }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, allow_blank: true, message: :enter_in_single_byte_numbers_without_hyphens }
   end
-  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1, message: :not_selected }
 
   def save
     purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
